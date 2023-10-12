d43a4045e965fddcd274fe16b7f53003a011819b
9mean3 <gminseong114@gmail.com>
2023-10-13 00:01:00 +0900

    good

--- Assets/01Scripts/Memory.cs
+++ Assets/01Scripts/Memory.cs
@@ -2,41 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum State
+{
+    Questioning,
+    Listening,
+}
+
 public class Memory : MonoBehaviour
 {
-    [SerializeField] List<GameObject> Obj;
-    Stack<int> callIdx;
+    State curState;
 
-    [SerializeField]
+    [SerializeField] List<GameObject> Obj = new List<GameObject>();
+    List<int> objOrder = new List<int>();
+    [SerializeField] float qCooltime;
+    [SerializeField] float qFlashtime;
+    int qCount = 1;
+    Camera mainCam;
+    bool isWrong = false;
 
 
-
-    private void Update()
+    private void Start()
     {
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            
-        }
+        mainCam = Camera.main;
+        curState = State.Questioning;
+        Question();
     }
 
-    IEnumerator Setc()
+    void Question()
     {
-        if(callIdx.Count != 0)
+        Obj.ForEach((GameObject obj) =>
         {
-            for (int i = 0; i < callIdx.Count; i++)
+            Color t = obj.GetComponent<SpriteRenderer>().color;
+            t.a = 0.1f;
+            obj.GetComponent<SpriteRenderer>().color = t;
+        });
+        StartCoroutine(QCor());
+    }
+
+    void Listen()
+    {
+        curState = State.Listening;
+        StartCoroutine(LCor());
+    }
+
+    IEnumerator QCor()
+    {
+        int cCnt = qCount;
+        objOrder.Add(Random.Range(0, Obj.Count));
+
+        while (cCnt > 0)
+        {
+            //Debug.Log(cCnt + ' ' + q);
+
+            yield return new WaitForSeconds(qCooltime);
+            int rn = qCount - cCnt;
+            Color t = Obj[objOrder[rn]].GetComponent<SpriteRenderer>().color;
+            t.a = 1f;
+            Obj[objOrder[rn]].GetComponent<SpriteRenderer>().color = t;
+            yield return new WaitForSeconds(qFlashtime);
+            t.a = 0.1f;
+            Obj[objOrder[rn]].GetComponent<SpriteRenderer>().color = t;
+
+            cCnt--;
+        }
+        qCount++;
+        Listen();
+    }
+
+    IEnumerator LCor()
+    {
+        yield return new WaitUntil(CKAnswer);
+        print(isWrong);
+        Question();
+    }
+
+    int cIdx = 0;
+    bool CKAnswer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                mainCam.ScreenToWorldPoint(Input.mousePosition),
+                mainCam.transform.forward,
+                20);
+            if (hit.collider != null && hit.transform.CompareTag("Node"))
             {
-                Obj[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
-                yield return new WaitForSeconds(0.1f);
-                Obj[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+                foreach (var item in Obj)
+                {
+                    if(item == hit.transform.gameObject && item == Obj[objOrder[cIdx]])
+                    {
+                        Color t = Obj[objOrder[cIdx]].GetComponent<SpriteRenderer>().color;
+                        t.a = 1f;
+                        Obj[objOrder[cIdx]].GetComponent<SpriteRenderer>().color = t;
+                        Wait(qFlashtime);
+                        t.a = 0.1f;
+                        Obj[objOrder[cIdx]].GetComponent<SpriteRenderer>().color = t;
+                        break;
+                    }
+                    else if(item == hit.transform.gameObject && item != Obj[objOrder[cIdx]])
+                    {
+                        isWrong = true;
+                        Debug.Log("Wrong!");
+                        return true;
+                    }
+                }
+
+                cIdx++;
             }
         }
-        int r = Random.Range(0, Obj.Count - 1);
-        callIdx.Push(r);
-        Obj[r].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
-        yield return new WaitForSeconds(0.1f);
-        Obj[r].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
 
+        if (cIdx >= objOrder.Count) {
+            cIdx = 0;
+            return true;
+        }
+        else return false;
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
     }
 }
